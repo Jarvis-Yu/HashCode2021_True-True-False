1b9640ccad37c984a16f0de8234bd64a7f5b27a8
JarvisYu <JarvisYu_Cn@outlook.com>
2021-02-21 23:56:35 +0000

    Main added .run() & .timedRun()

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,21 @@ import io.Formatter;
 import io.Parser;
 
 public class Main {
-  public static void main(String[] args) {
+
+  public static void timedRun() {
+    long timer = System.currentTimeMillis();
+    run();
+    long duration = System.currentTimeMillis() - timer;
+    System.out.printf("%nRuntime: %s seconds%n", duration / 1000.0);
+  }
+
+  public static void run() {
     InStructure input = Parser.parser();
     OutStructure output = null; // algorithms.TODO(input);
     Formatter.formatter(output);
+  }
+
+  public static void main(String[] args) {
+    timedRun();
   }
 }
