0374de581b67c9684e6118d612d7c2a36314d057
tony <tony_t_liu@outlook.com>
2021-02-26 03:08:09 +0800

    在写formattor

--- src/dataClasses/out/Solution.java
+++ src/dataClasses/out/Solution.java
@@ -2,7 +2,15 @@ package dataClasses.out;
 
 import dataClasses.in.Intersection;
 
+import java.util.List;
+
 public class Solution {
   public int numberOfIntersections;
   public List<Intersection> listOfIntersection;
+  public int numOfScheduledIntersections(){
+    int n = 0;
+    for (Intersection inter : listOfIntersection){
+
+    }
+  }
 }
