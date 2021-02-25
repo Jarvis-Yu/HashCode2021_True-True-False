277b0ce0f59c7da1fb891bc9d3c802b891c053de
tony <tony_t_liu@outlook.com>
2021-02-26 03:19:16 +0800

    正在写solution 和 输出 formattor

--- src/dataClasses/out/Solution.java
+++ src/dataClasses/out/Solution.java
@@ -7,10 +7,15 @@ import java.util.List;
 public class Solution {
   public int numberOfIntersections;
   public List<Intersection> listOfIntersection;
+
   public int numOfScheduledIntersections(){
     int n = 0;
     for (Intersection inter : listOfIntersection){
-
+      if (!inter.isUnchanged()) {
+        // 如果有变化
+        n += 1;
+      }
     }
+    return n;
   }
 }
