99cebbb505083e09072d6bf2c6daf338d62ea355
JarvisYu <JarvisYu_Cn@outlook.com>
2021-02-25 18:58:06 +0000

    update Instructure & Street

--- src/dataClasses/out/Solution.java
+++ src/dataClasses/out/Solution.java
@@ -2,6 +2,8 @@ package dataClasses.out;
 
 import dataClasses.in.Intersection;
 
+import java.util.List;
+
 public class Solution {
   public int numberOfIntersections;
   public List<Intersection> listOfIntersection;
