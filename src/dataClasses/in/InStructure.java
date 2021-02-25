167802079c8c1534616b4ff82b856e1bc76ca830
JarvisYu <JarvisYu_Cn@outlook.com>
2021-02-25 18:54:22 +0000

    update Instructure & Street

--- src/dataClasses/in/InStructure.java
+++ src/dataClasses/in/InStructure.java
@@ -1,11 +1,16 @@
 package dataClasses.in;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class InStructure {
 
   public Map<String, Street> steetMap = new HashMap<>();
+  public List<Intersection> intersections = new ArrayList<>();
+
+  public InStructure() {}
 
   public void addStreet(Street street) {};
 }
