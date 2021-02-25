ac9f3f07d18b27e73724d508d1cfbb145771b4f1
tony <tony_t_liu@outlook.com>
2021-02-26 03:12:11 +0800

    comment 了 Intersection

--- src/dataClasses/in/Intersection.java
+++ src/dataClasses/in/Intersection.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Intersection {
 
+	// 如果不对 intersection 不进行修改，那么就是两个空 list
+
 	public List<String> streets = new ArrayList<>();
 	public List<Integer> durations = new ArrayList<>();
 
