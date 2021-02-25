b0a68fe42de48a67f23fde824ff615981c4c30a1
JarvisYu <JarvisYu_Cn@outlook.com>
2021-02-25 11:44:41 +0000

    gitScript/* added comments

--- gitScript/log.sh
+++ gitScript/log.sh
@@ -1,1 +1,2 @@
+# Display logs in the form: hashcode, author, committed t time ago, description
 git log --pretty=format:"%h - %an, %ar : %s" --graph
--- gitScript/simpleLog.sh
+++ gitScript/simpleLog.sh
@@ -1,1 +1,2 @@
+# Display logs in the form: hashcode, description
 git log --pretty=format:"%h - %s" --graph
