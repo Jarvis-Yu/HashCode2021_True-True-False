5807760b23703b9118138dacb64f3f023c1381c6
JarvisYu <JarvisYu_Cn@outlook.com>
2021-02-25 18:30:10 +0000

    added Car, Street

---
+++ src/dataClasses/in/Car.java
@@ -0,0 +1,4 @@
+package dataClasses.in;
+
+public class Car {
+}
---
+++ src/dataClasses/in/Street.java
@@ -0,0 +1,20 @@
+package dataClasses.in;
+
+import dataClasses.Pair;
+
+import java.util.Queue;
+
+public class Street {
+
+  public Queue<Pair<Car, Integer>> drivingCar;
+  public Queue<Car> queueCars;
+  public final Integer drivingTime;
+  public final TrafficLight startIntersection;
+  public final TrafficLight finalIntersection;
+
+  public Street(TrafficLight startIntersection, TrafficLight finalIntersection, Integer drivingTime) {
+    this.startIntersection = startIntersection;
+    this.finalIntersection = finalIntersection;
+    this.drivingTime = drivingTime;
+  }
+}
