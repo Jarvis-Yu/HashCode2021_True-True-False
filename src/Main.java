0f68a2071380844b3195e3279cc0fc5fc10cf60d
tony <tony_t_liu@outlook.com>
2021-02-26 02:45:05 +0800

    comment 掉了 main 改了 formatter

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ import io.Parser;
 
 public class Main {
 
-  public static void timedRun() {
-    long timer = System.currentTimeMillis();
-    run();
-    long duration = System.currentTimeMillis() - timer;
-    System.out.printf("%nRuntime: %s seconds%n", duration / 1000.0);
-  }
-
-  public static void run() {
-    InStructure input = Parser.parser();
-    Solution output = null; // algorithms.TODO(input);
-    Formatter.formatter(output);
-  }
-
-  public static void main(String[] args) {
-    timedRun();
-  }
+//  public static void timedRun() {
+//    long timer = System.currentTimeMillis();
+//    run();
+//    long duration = System.currentTimeMillis() - timer;
+//    System.out.printf("%nRuntime: %s seconds%n", duration / 1000.0);
+//  }
+//
+//  public static void run() {
+//    InStructure input = Parser.parser();
+//    Solution output = null; // algorithms.TODO(input);
+//    Formatter.formatter(output);
+//  }
+//
+//  public static void main(String[] args) {
+//    timedRun();
+//  }
 }
