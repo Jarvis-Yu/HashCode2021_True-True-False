2342a9de2dfeffdc4c6120f6927b90e1e5665125
Yitang Chen <yc4120@ic.ac.uk>
2021-02-26 04:31:11 +0800

    Merge branch 'master' of github.com:Jarvis-Yu/TrueOrFalse

--- src/dataClasses/in/Intersection.java
+++ src/dataClasses/in/Intersection.java
@@ -9,14 +9,16 @@ public class Intersection {
 
 	public List<String> streets = new ArrayList<>();
 	public List<Integer> durations = new ArrayList<>();
+	public InStructure ins;
 
 	public boolean isUnchanged(){
 		return this.streets.isEmpty() && this.durations.isEmpty();
 	}
 
-	public Intersection(List<String> streets, List<Integer> durations) {
+	public Intersection(List<String> streets, List<Integer> durations, InStructure ins) {
 		this.streets = streets;
 		this.durations = durations;
+		this.ins = ins;
 	}
 
 	@Override
@@ -29,5 +31,22 @@ public class Intersection {
 
 	public int numOfScheduledStreets(){
 		return streets.size();
+	}
+
+		public void simulate(int time) {
+		var mod = time % round();
+		int i = 0;
+		while (true) {
+			mod = mod - durations.get(i);
+			if (mod < 0) {
+				break;
+			}
+			i++;
+		}
+		ins.streetMap.get(streets.get(i)).nextCarLeave(time);
+	}
+
+	private int round() {
+		return durations.stream().reduce(Integer::sum).get();
 	}
 }
